7d10c9ca2fcd2f9e2da9e5f23270f06c03c56a3e
CatMusty <jorji@mymail.mines.edu>
2019-11-21 00:01:54 -0700

    Week12-utility.py

--- Week12-utility.py
+++ Week12-utility.py
@@ -22,3 +22,10 @@ def UpdateString(stringUno, stringDos, num):
     for i in string_list:
         final_string += i
     print('OUTPUT {final}'.format(final=final_string))
+
+def FindWordCount(issalist, issastring):
+    list_string = ''
+    for i in issalist:
+        list_string += i
+    count = list_string.count(issastring)
+    print('OUTPUT %d' % (count))
